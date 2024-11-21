dcab1cb41d8bf7b55c20917aacb1aa9a6cd804e4
AmaseCocoa <amase.cocoa@gmail.com>
2024-11-21 20:59:47 +0900

    v0.2.3

--- src/midist/cli.py
+++ src/midist/cli.py
@@ -15,7 +15,7 @@ pl_th_list_tmpl = """
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial, initial-scale=1.0">
     <title>{{ tmpl_type }} List</title>
     <meta name="generator" content="AmaseCocoa/MiDist (v{{ __version__ }})">
 </head>
@@ -43,6 +43,10 @@ pl_th_list_tmpl = """
 </html>
 """
 
+def replace_path_separators(path):
+    """パスの区切り文字を'-'に置き換え"""
+    return path.replace(os.sep, '-')
+
 @app.command(name="generate")
 def generate(base_url: str, dir: str = "./"):
     if not os.path.exists(dir):
@@ -63,50 +67,55 @@ def generate(base_url: str, dir: str = "./"):
         for root, dirs, files in os.walk(base_dir):
             for file in files:
                 file_path = os.path.join(root, file)
-                single_line, file_hash = process_file(file_path)
-                relative_path = os.path.relpath(file_path, start=base_dir)
-                json_dir = os.path.dirname(relative_path)
-                json_file_path = os.path.join(themes_dir, json_dir)
-                os.makedirs(json_file_path, exist_ok=True)
-                theme_name = os.path.splitext(os.path.basename(file_path))[0]
-                themes.append(
-                    {
-                        "name": theme_name,
-                        "link": f"https://misskey-hub.net/mi-web?path={urllib.parse.quote(f'/install-extensions?url={base_url}/api/themes/{json_dir}/{theme_name}.json&hash={file_hash}')}",
-                    }
-                )
-
-                with open(os.path.join(json_file_path, f"{theme_name}.json"), "w") as f:
-                    json.dump(
-                        {"type": "theme", "data": single_line},
-                        f,
-                        ensure_ascii=False,
-                        indent=4,
+                if not os.path.basename(file_path).lower().endswith(".md") or os.path.basename(file_path).lower().endswith(".txt"):
+                    single_line, file_hash = process_file(file_path)
+                    relative_path = os.path.relpath(file_path, start=base_dir)
+                    json_dir = os.path.dirname(relative_path)
+                    json_file_path = os.path.join(themes_dir, json_dir)
+                    os.makedirs(json_file_path, exist_ok=True)
+                    theme_name = replace_path_separators(json_dir)
+                    theme_name = f"{theme_name}-{os.path.splitext(os.path.basename(file_path))[0]}"
+                    themes.append(
+                        {
+                            "name": theme_name,
+                            "link": f"https://misskey-hub.net/mi-web?path={urllib.parse.quote(f'/install-extensions?url={base_url}/api/themes/{json_dir}/{theme_name}.json&hash={file_hash}')}",
+                        }
                     )
+
+                    with open(os.path.join(json_file_path, f"{theme_name}.json"), "w") as f:
+                        json.dump(
+                            {"type": "theme", "data": single_line},
+                            f,
+                            ensure_ascii=False,
+                            indent=4,
+                        )
 
     def process_plugins(base_dir):
         for root, dirs, files in os.walk(base_dir):
             for file in files:
                 file_path = os.path.join(root, file)
-                single_line, file_hash = process_file(file_path)
-                relative_path = os.path.relpath(file_path, start=base_dir)
-                json_dir = os.path.dirname(relative_path)
-                json_file_path = os.path.join(plugins_dir, json_dir)
-                os.makedirs(json_file_path, exist_ok=True)
-                plugin_name = os.path.splitext(os.path.basename(file_path))[0]
-                plugins.append(
-                    {
-                        "name": plugin_name,
-                        "link": f"https://misskey-hub.net/mi-web?path={urllib.parse.quote(f'{base_url}/api/plugins/{json_dir}/{plugin_name}.json?hash={file_hash}')}",
-                    }
-                )
-                with open(os.path.join(json_file_path, f"{plugin_name}.json"), "w") as f:
-                    json.dump(
-                        {"type": "plugin", "data": single_line},
-                        f,
-                        ensure_ascii=False,
-                        indent=4,
+                if not os.path.basename(file_path).lower().endswith(".md") or os.path.basename(file_path).lower().endswith(".txt"):
+                    single_line, file_hash = process_file(file_path)
+                    relative_path = os.path.relpath(file_path, start=base_dir)
+                    json_dir = os.path.dirname(relative_path)
+                    json_file_path = os.path.join(plugins_dir, json_dir)
+                    os.makedirs(json_file_path, exist_ok=True)
+                    # ディレクトリ名から"api/plugins/"を除いて'-'で置き換える
+                    plugin_name = replace_path_separators(json_dir)
+                    plugin_name = f"{plugin_name}-{os.path.splitext(os.path.basename(file_path))[0]}"
+                    plugins.append(
+                        {
+                            "name": plugin_name,
+                            "link": f"https://misskey-hub.net/mi-web?path={urllib.parse.quote(f'{base_url}/api/plugins/{json_dir}/{plugin_name}.json?hash={file_hash}')}",
+                        }
                     )
+                    with open(os.path.join(json_file_path, f"{plugin_name}.json"), "w") as f:
+                        json.dump(
+                            {"type": "plugin", "data": single_line},
+                            f,
+                            ensure_ascii=False,
+                            indent=4,
+                        )
 
     process_themes(os.path.join(dir, "themes"))
     process_plugins(os.path.join(dir, "plugins"))
